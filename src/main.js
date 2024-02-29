846a2f61a56406809958f4f762f3d345bcc6d70a
shdg37844 <missatail886@gmail.com>
2024-02-29 18:49:47 +0800

    add new files

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import './assets/css/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+import 'element-plus/dist/index.css'
+import 'virtual:svg-icons-register'
+import SvgIcon from '@/components/Common/SvgIcon.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.component('SvgIcon', SvgIcon)
+app.mount('#app')
